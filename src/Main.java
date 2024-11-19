1e833220302b2f93513dc2e6c8f2fadb65a90f45
Claudio-Oumar <133616530+Claudio-Oumar@users.noreply.github.com>
2024-11-19 11:15:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,16 @@ import javax.swing.*;
 
 public class Main {
 
-    // Atributo para la ventana principal
     private static VentanaPrincipal ventanaPrincipal;
 
     public static void main(String[] args) {
-        iniciarAplicacion(); // Método para iniciar la aplicación
+        iniciarAplicacion(); 
     }
 
-    // Método para iniciar la aplicación
     private static void iniciarAplicacion() {
-        // Inicia la aplicación en el hilo de eventos de Swing para asegurar una experiencia de UI fluida.
         SwingUtilities.invokeLater(() -> {
-            ventanaPrincipal = new VentanaPrincipal();  // Inicializa la ventana principal como atributo
-            ventanaPrincipal.setVisible(true);          // Hacer visible la ventana
+            ventanaPrincipal = new VentanaPrincipal(); 
+            ventanaPrincipal.setVisible(true);         
         });
     }
 }
